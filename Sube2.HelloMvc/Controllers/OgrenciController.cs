6a12f49feb7a8eb045254434f340f1d6da1df176
Gazi <Gazi@DESKTOP-224SE1B>
2024-03-18 16:00:10 +0300

    Add project files.

---
+++ Sube2.HelloMvc/Controllers/OgrenciController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sube2.HelloMvc.Models;
+
+namespace Sube2.HelloMvc.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public ViewResult Index()//Action
+        {
+            return View();
+        }
+
+        public string OgrenciDetay(int id)
+        {
+            Ogrenci ogr = null;
+            if (id == 1)
+            {
+                ogr = new Ogrenci();
+                ogr.Ad = "Ali";
+                ogr.Soyad = "Veli";
+                ogr.Numara = 123;
+            }
+            else if (id == 2)
+            {
+                ogr = new Ogrenci { Ad = "Ahmet", Soyad = "Mehmet", Numara = 456 };
+            }
+            return ogr.ToString();
+        }
+    }
+}
---
+++ Sube2.HelloMvc/Models/Ogrenci.cs
@@ -0,0 +1,14 @@
+﻿namespace Sube2.HelloMvc.Models
+{
+    public class Ogrenci:Object
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Numara { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ad:{this.Ad}-Soyad:{Soyad}-Numara:{Numara}";
+        }
+    }
+}
