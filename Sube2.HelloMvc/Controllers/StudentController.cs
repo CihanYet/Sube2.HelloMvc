eb6efe73bd1f46b46cf72a0d4ae4a046aa8967e1
Gazi <Gazi@DESKTOP-224SE1B>
2024-06-03 16:30:30 +0300

    EF CRUD

--- Sube2.HelloMvc/Controllers/StudentController.cs
+++ Sube2.HelloMvc/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Sube2.HelloMvc.Models;
 
 namespace Sube2.HelloMvc.Controllers
@@ -7,7 +8,11 @@ namespace Sube2.HelloMvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            using (var ctx = new OkulDbContext())
+            {
+                var lst = ctx.Ogrenciler.ToList();
+                return View(lst);
+            }
         }
 
         [HttpGet]
@@ -27,7 +32,40 @@ namespace Sube2.HelloMvc.Controllers
                     ctx.SaveChanges();
                 }
             }
-            return View();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditStudent(int id)
+        {
+            using (var ctx = new OkulDbContext())
+            {
+                var ogr = ctx.Ogrenciler.Find(id);
+                return View(ogr);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EditStudent(Ogrenci ogr)
+        {
+            if (ogr != null)
+            {
+                using (var ctx = new OkulDbContext())
+                {
+                    ctx.Entry(ogr).State = EntityState.Modified;
+                    ctx.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteStudent(int id)
+        {
+            using (var ctx = new OkulDbContext())
+            {
+                ctx.Ogrenciler.Remove(ctx.Ogrenciler.Find(id));
+                ctx.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
